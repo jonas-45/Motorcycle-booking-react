55c2195979e62263a19c74ed86216f9eaa94b43f
ehmaddd <ehmaddd@gmail.com>
2023-09-02 12:37:06 +0500

    Create Navigationbar and blank pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,9 @@
 import { NavLink, Routes, Route } from 'react-router-dom';
 import Motorcycles from './Motorcycles';
+import Reserve from './Reserve';
+import Reservations from './Reservations';
+import AddMotorcycle from './AddMotorcycle';
+import DeleteMotorcycle from './DeleteMotorcycle';
 
 const Navbar = () => (
   <nav>
@@ -9,9 +13,33 @@ const Navbar = () => (
           Motorcycles
         </NavLink>
       </li>
+      <li>
+        <NavLink to="/reserve">
+          Reserve
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/reservations">
+          My Reservations
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/add-motorcycle">
+          Add Motorcycle
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/delete-motorcycle">
+          Delete Motorcycle
+        </NavLink>
+      </li>
     </ul>
     <Routes>
       <Route path="/motorcycles" element={<Motorcycles />} />
+      <Route path="/reserve" element={<Reserve />} />
+      <Route path="/reservations" element={<Reservations />} />
+      <Route path="/add-motorcycle" element={<AddMotorcycle />} />
+      <Route path="/delete-motorcycle" element={<DeleteMotorcycle />} />
     </Routes>
   </nav>
 );
---
+++ src/components/Reservations.js
@@ -0,0 +1,7 @@
+const Reservations = () => {
+    return (
+        <h1>My Reservations</h1>
+    )
+}
+
+export default Reservations;
