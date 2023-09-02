974b99401e4da3d9d46e1112870b93608903dd38
ehmaddd <ehmaddd@gmail.com>
2023-09-02 09:59:47 +0500

    Add Authentication rule

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function Login() {
+  const [credentials, setCredentials] = useState({ username: ''});
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const queryParams = {
+    username: credentials.username
+  };
+
+  const queryString = new URLSearchParams(queryParams).toString();
+  const url = `http://localhost:3000/api/users/login?${queryString}`;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(url);
+      console.log('User Found');
+    } catch (error) {
+      console.log('No such user');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="username"
+          name="username"
+          placeholder="username"
+          value={credentials.username}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default Login;
