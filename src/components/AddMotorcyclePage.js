45080353d9d4543e18cc927b26780c1a0c35386a
unknown <patrickkeita80@yahoo.com>
2023-09-04 02:48:07 -0700

    Add AddMotorcyclePage.js

---
+++ src/components/AddMotorcyclePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AddMotorcycle from './navigation/AddMotorcycle';
+
+const AddMotorcyclePage = () => (
+
+  <AddMotorcycle />
+);
+
+export default AddMotorcyclePage;
